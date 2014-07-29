ff22c61bdb17726f1b48228cf7e3c04ac42d256f
Nelson Loyola <loyola@ualberta.ca>
2014-07-29 11:28:27 -0600

    removed files after merging with Connor's branch.

--- server/server/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-                       url(r'^', include('freezers.urls')),
-                       url(r'^', include('specifications.urls')),
-)
